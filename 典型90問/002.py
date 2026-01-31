93b88cd10c45e24b1b0da2f3537860144e44e3da
shu0219at <at398584@gmail.com>
2026-01-31 10:21:08 +0900

    add 002

--- 典型90問/002.py
+++ 典型90問/002.py
@@ -1,2 +1,14 @@
 #002 - Encyclopedia of Parentheses
+
+def solve(s, open_cnt, close_cnt, N):
+	if len(s) == N:
+		print(s)
+		return
+	if open_cnt < N // 2:
+		solve(s + '(', open_cnt + 1, close_cnt, N)
+	if close_cnt < open_cnt:
+		solve(s + ')', open_cnt, close_cnt + 1, N)
+
 N = int(input())
+if N % 2 == 0:
+	solve("", 0, 0, N)
