03897b6f70ec20317b2e467e633b7938cb198c60
shu120 <sk_0219@icloud.com>
2026-09-19 01:22:20 +0900

    add SegTree into LIB

--- LIB/lib.py
+++ LIB/lib.py
@@ -7,7 +7,7 @@ def compress(A):
 
 
 def dfs(start, G, N):
-    """DFS(stack): startからの各頂点への距離を返す（到達不能は-1）"""
+    """DFS(stack): startからのDFS木上の深さを返す（未訪問は-1）"""
     dist = [-1] * N
     dist[start] = 0
     stack = [start]
@@ -126,33 +126,97 @@ def dijkstra(G, start):
 
 
 class SegTree:
-    def __init__(self, A):
-        n = len(A)
-        self.size = 1
-        while self.size < n:
-            self.size <<= 1
-        self.data = [0] * (2 * self.size)
+    """Segment Tree
+    0-indexed / 点更新・区間クエリ
 
-        # 葉
-        for i in range(n):
+    Parameters
+    ----------
+    A : list
+        初期配列
+    op : function
+        2つの区間情報を結合する関数
+    e : any
+        opの単位元
+
+    Methods
+    -------
+    update(i, x)
+        A[i] を x に変更する
+    query(left, right)
+        閉区間 [left, right] の情報を取得する
+    all_query()
+        配列全体の情報を取得する
+    """
+
+    def __init__(self, A, op, e):
+        self.op = op
+        self.e = e
+        self.n = len(A)
+
+        # 葉の数を n 以上の最小の2冪にする
+        self.size = 1
+        while self.size < self.n:
+            self.size <<= 1
+
+        # data[1] が根、data[size:] が葉
+        self.data = [e] * (2 * self.size)
+
+        # 葉に初期値を入れる
+        for i in range(self.n):
             self.data[self.size + i] = A[i]
 
-        # 構築
+        # 下から順に親を構築する
         for i in range(self.size - 1, 0, -1):
-            self.data[i] = max(self.data[2*i], self.data[2*i+1])
+            self.data[i] = self.op(
+                self.data[2 * i],
+                self.data[2 * i + 1]
+            )
 
-    def query(self, _l, r):
-        # [l, r] の最大値
-        _l += self.size
-        r += self.size
-        res = 0
-        while _l <= r:
-            if _l & 1:
-                res = max(res, self.data[_l])
-                _l += 1
-            if not (r & 1):
-                res = max(res, self.data[r])
-                r -= 1
-            _l >>= 1
-            r >>= 1
-        return res
+    def update(self, i, x):
+        """A[i] を x に変更する"""
+        i += self.size
+        self.data[i] = x
+
+        # 更新した葉から根まで再計算
+        while i > 1:
+            i >>= 1
+            self.data[i] = self.op(
+                self.data[2 * i],
+                self.data[2 * i + 1]
+            )
+
+    def query(self, left, right):
+        """閉区間 [left, right] の情報を取得する"""
+        left += self.size
+        right += self.size
+
+        # opが非可換でも使えるよう左右を分けて集計
+        res_left = self.e
+        res_right = self.e
+
+        while left <= right:
+            # left が右の子なら、その区間を採用
+            if left & 1:
+                res_left = self.op(
+                    res_left,
+                    self.data[left]
+                )
+                left += 1
+
+            # right が左の子なら、その区間を採用
+            if not (right & 1):
+                res_right = self.op(
+                    self.data[right],
+                    res_right
+                )
+                right -= 1
+
+            # 親へ移動
+            left >>= 1
+            right >>= 1
+
+        return self.op(res_left, res_right)
+
+    def all_query(self):
+        """配列全体の情報を取得する"""
+        return self.data[1]
